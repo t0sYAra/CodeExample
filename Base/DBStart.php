a7ca15e55cb4e88119bb26c36996e2a664f356b6
Anton Pavlov <mail@antonpavlov.ru>
2017-12-04 07:22:51 +0300

    Removed passwords from db connection :)

--- Base/DBStart.php
+++ Base/DBStart.php
@@ -12,7 +12,7 @@ trait DBStart
         $db = $reg->get('db');
         try {
             if (!isset($db)) {
-                $db = new \PDO('mysql:host=localhost;dbname=ap', 'apdbuser', 'J3nBs0ckDsn4Pxl2');
+                $db = new \PDO('mysql:host=localhost;dbname=ap', 'user', 'pass');
                 $reg->set('db',$db);
             }
         } catch(PDOException $e) {
